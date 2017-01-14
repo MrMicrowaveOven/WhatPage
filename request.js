5dc132ecf3e3b573ee87635b5ffd73c2fb32d0a5
Benjamin <benj@zagorski.com>
2017-01-13 20:15:32 -0800

    App now makes calls to API and returns search results

--- request.js
+++ request.js
@@ -2,18 +2,64 @@
 
 function makeRequest() {
   var keyword = document.getElementById("keyword").value;
-  var parameters = document.getElementById("parameters").value;
+  var parameters = document.getElementById("parameters").value.split(" ").join("+");
+  request_for_nth_page(1, keyword, parameters);
+}
+
+function request_for_nth_page(n, keyword, parameters) {
+  if(n>10){
+    console.log("Beyond the 10th page");
+    return;
+  }
+  var found = false;
+  var links = [];
   $.ajax({
     type: "GET",
     url: "https://www.googleapis.com/customsearch/v1",
     data: {
         key: "AIzaSyD7nKgEWQCBHw1uGJOEvXN-ae4cMeOMXuE",
-        cs: "012667598935678745968:ixjc-b6xzre",
-        fields: "items(title)",
-        q: "custom+email"
+        cx: "012667598935678745968:ixjc-b6xzre",
+        fields: "items(link)",
+        q: "Brad Pitt",
+        start: (n - 1) * 10 + 1
     },
     success: function (res) {
-        document.getElementById("response").innerHTML = res;
+        // console.log(res.items.length);
+        res.items.forEach(function (item) {
+          links.push(item.link);
+        });
+        links.forEach(function (link, ind) {
+          var numCheck = (n - 1) * 10 + 1 + ind;
+          console.log("Checking " + numCheck);
+          if (link.indexOf("imdb") !== -1) {
+            document.getElementById("response").innerHTML = numCheck;
+            found = true;
+          }
+        });
+        if(found === false){
+          askForNextStep(n, keyword, parameters);
+        }
+    },
+    error: function (xhr, status, error) {
+      console.log(xhr);
+      console.log(status);
+      console.log(error);
+      if (xhr.status === 403) {
+        document.getElementById("response").innerHTML = "Sorry, ran out of pings for today :(";
+      }
     }
-});
+  });
+
+}
+function askForNextStep(n, keyword, parameters) {
+  var first = n;
+  var last = n + 9;
+  document.getElementById("response").innerHTML =
+    "I've checked results " + first + "through " + last +
+    ".  Shall I keep looking?";
+  document.getElementById("keepGoing").Style.visibility = "visible";
+}
+
+function keepGoing() {
+
 }

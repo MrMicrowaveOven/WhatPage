c06de3413aa1dc56140877bee418fad9e13b7d4a
Benjamin <benj@zagorski.com>
2017-01-14 13:14:19 -0800

    App will now tell you which of the first 10 Google Search results contains the keyword in its url

--- request.js
+++ request.js
@@ -13,25 +13,28 @@ function request_for_nth_page(n, keyword, parameters) {
   }
   var found = false;
   var links = [];
+  var searchParameters = parameters.split(" ").join("+");
+  // keyword = "google";
   $.ajax({
     type: "GET",
     url: "https://www.googleapis.com/customsearch/v1",
     data: {
         key: "AIzaSyD7nKgEWQCBHw1uGJOEvXN-ae4cMeOMXuE",
         cx: "012667598935678745968:ixjc-b6xzre",
-        fields: "items(link)",
-        q: "Brad Pitt",
+        fields: "items(link),queries",
+        q: searchParameters,
         start: (n - 1) * 10 + 1
     },
     success: function (res) {
-        // console.log(res.items.length);
+        console.log("Starting at " + res.queries.request[0].startIndex);
         res.items.forEach(function (item) {
           links.push(item.link);
         });
         links.forEach(function (link, ind) {
           var numCheck = (n - 1) * 10 + 1 + ind;
           console.log("Checking " + numCheck);
-          if (link.indexOf("imdb") !== -1) {
+          console.log(link);
+          if (link.indexOf(keyword) !== -1) {
             document.getElementById("response").innerHTML = numCheck;
             found = true;
           }
@@ -57,7 +60,7 @@ function askForNextStep(n, keyword, parameters) {
   document.getElementById("response").innerHTML =
     "I've checked results " + first + "through " + last +
     ".  Shall I keep looking?";
-  document.getElementById("keepGoing").Style.visibility = "visible";
+  document.getElementById("keepGoing").style.visibility = "visible";
 }
 
 function keepGoing() {
--- response.js
+++ response.js
@@ -3,9 +3,9 @@
 var res = {
  "kind": "customsearch#search",
  "url": {
-  "type": "application/json",
-  "template": "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&cref={cref?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json"
- },
+    "type": "application/json",
+    "template": "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&cref={cref?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json"
+   },
  "queries": {
   "request": [
    {
@@ -541,4 +541,4 @@ var res = {
 
  ]
 };
-console.log(res);
+console.log(res.queries.request[0].startIndex);
